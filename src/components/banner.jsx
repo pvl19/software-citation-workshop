c055e1c49fa9df1c2ada5b30a66d69e9a750f8cc
Phil Van-Lane <philv19@gmail.com>
2026-03-02 16:26:59 -0800

    added dates and location to banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -8,7 +8,10 @@ export default function Banner() {
       <div className='scs-logo'>
         <img src="/resources/software-citation-station-no-circle-blue.png" alt="Software Citation Logo" className="scs-logo-img" />
       </div>
+      <div className='workshop-title'>
       <h1>Software Citation Workshop</h1>
+      <h2>April 13-16, 2026 | San Diego Supercomputer Center (UCSD)</h2>
+      </div>
     </header>
     </div>
   )
